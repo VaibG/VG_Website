844e4f9cb7e4e09f2c09b48ca84027663c0d6ef1
Vaibhav Gattani <vaibg@berkeley.edu>
2019-08-04 23:29:11 +0800

    Updates

---
+++ precache-manifest.6852bef91c07bf85dd51a06d6892c515.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267ae6d819a8df36919a897caa63f43f",
+    "url": "/VG_Website/index.html"
+  },
+  {
+    "revision": "256b104988814713296a",
+    "url": "/VG_Website/static/css/2.a9a8df71.chunk.css"
+  },
+  {
+    "revision": "da1382f102c905e88583",
+    "url": "/VG_Website/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "256b104988814713296a",
+    "url": "/VG_Website/static/js/2.1f0559d9.chunk.js"
+  },
+  {
+    "revision": "da1382f102c905e88583",
+    "url": "/VG_Website/static/js/main.5e1362f3.chunk.js"
+  },
+  {
+    "revision": "98c09e725562200b04f8",
+    "url": "/VG_Website/static/js/runtime~main.bb89d132.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/VG_Website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/VG_Website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/VG_Website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/VG_Website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/VG_Website/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
